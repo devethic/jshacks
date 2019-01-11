4de99976757af78b19c177305fc3984750a7ad6d
devethic <devethic@gmail.com>
2019-01-11 21:23:39 +0100

    FIX ne s'arrête plus si un téléchargement est en cours FIX clique sur le bouton "sauvegarder" au lieu du bouton de téléchargement ADD 1fichier prise en compte du mot de passe : "annuaire-telechargement.com"

--- content.js
+++ content.js
@@ -44,19 +44,33 @@ function uptobox () {
 }
 
 function unFichier () {
+
+  // do nothing if pending download ?
+  let d = document.querySelector("form>div.ct_warn");
+  if (d && /Sans souscription/i.test(d.innerText)) {
+    return;
+  }
+
   // simulate click on 1st download button
-  let b = document.querySelector("input[type=submit]");
-  if (b) {
-    // do nothing if pending download ?
-    let d = document.querySelector("form>div>div");
-    if (d && /un seul fichier à la fois/i.test(d.innerText))
-      return;
-    // do nothing if waiting counter
-    for (let c of document.querySelectorAll(".clock.flip-clock-wrapper>.flip.play")) {
-      if (c.querySelector(".flip-clock-active .inn").innerText != "0")
-      return;
+  let b = document.querySelectorAll("input[type=submit]");
+  if (b && b.length) {
+    // ensure submit button is the last and is not a save button
+    b = b[b.length-1];
+    if (!/sauvegarder/i.test(b.value)) {
+      // fill password if there
+      let e = document.querySelector("input#pass.input-text.ui-corner-all[type=password]");
+      if (e) {
+        // check if the password has been rejected
+        // stop if it's the case
+        let f = document.querySelectorAll("div.ct_warn");
+        if (f && f.length && f[f.length-1] == "L'accès à ce fichier est protégé par un mot de passe") {
+          return;
+        }
+        // set password
+        e.value = "annuaire-telechargement.com";
+      }
+      b.click();
     }
-    b.click();
   }
   // simulate click on second download button
   let a = document.querySelector("a.ok.btn-general.btn-orange");
